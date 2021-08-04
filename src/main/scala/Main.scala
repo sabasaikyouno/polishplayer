dc8a85e04e4b00ae1a2fce49a1c6e2a0a4cfe324
sabasaikyouno <0852kuro@gmail.com>
2021-08-04 16:36:39 +0900

    リファクタリング

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -41,10 +41,6 @@ class Main extends Application {
   override def start(primaryStage: Stage): Unit = {
     
     val videoStack = new StackPane()
-    val toolBar = new VBox()
-    val toolBarHBox = new HBox(5)
-    val titleBar = new StackPane()
-    val titleButton = new HBox()
 
     // ウィンドウ設定
     videoStack.setPrefSize(800,600)
@@ -55,149 +51,15 @@ class Main extends Application {
     videoImageView.fitWidthProperty().bind(videoStack.widthProperty())
     videoImageView.fitHeightProperty().bind(videoStack.heightProperty())
 
-    // ツールバー作成
-    // 時間表示設定
-    val timeLabel = new Label
-    timeLabel.setText(timeFmt.fmt(0))
-    timeLabel.setTextFill(Color.WHITE)
-
-    // 再生、停止ボタン
-    val playButtonImage = new Image(getClass.getResourceAsStream("play.png"))
-    val stopButtonImage = new Image(getClass.getResourceAsStream("stop.png"))
-    val pauseButton = new Button()
-    pauseButton.setGraphic(new ImageView(stopButtonImage))
-    pauseButton.setStyle("-fx-background-color:Transparent;-fx-background-radius:0")
-    pauseButton.setOnMouseClicked(event => {
-      embeddedMediaPlayer.controls().pause()
-      if (embeddedMediaPlayer.status().isPlaying){
-        pauseButton.setGraphic(new ImageView(playButtonImage))
-      } else {
-        pauseButton.setGraphic(new ImageView(stopButtonImage))
-      }
-    })
-
-    // forward ボタン
-    val forwardButtonImage = new Image(getClass.getResourceAsStream("forward.png"))
-    val forwardButton = new Button()
-    forwardButton.setGraphic(new ImageView(forwardButtonImage))
-    forwardButton.setStyle("-fx-background-color:Transparent;-fx-background-radius:0")
-    forwardButton.setOnMouseClicked(event => {
-      embeddedMediaPlayer.controls().skipTime(10000)
-    })
-
-    // back ボタン
-    val backButtonImage = new Image(getClass.getResourceAsStream("back.png"))
-    val backButton = new Button()
-    backButton.setGraphic(new ImageView(backButtonImage))
-    backButton.setStyle("-fx-background-color:Transparent;-fx-background-radius:0")
-    backButton.setOnMouseClicked(event => {
-      embeddedMediaPlayer.controls().skipTime(-10000)
-    })
-
-    // フルスクリーンボタン
-    val fullscreenReleaseButtonImage = new Image(getClass.getResourceAsStream("fullscreenRelease.png"))
-    val fullscreenButtonImage = new Image(getClass.getResourceAsStream("fullscreen.png"))
-    val fullscreenButton = new Button()
-    fullscreenButton.setGraphic(new ImageView(fullscreenButtonImage))
-    fullscreenButton.setStyle("-fx-background-color:Transparent;-fx-background-radius:0")
-    fullscreenButton.setOnMouseClicked(event => {
-      if (primaryStage.isFullScreen){
-        primaryStage.setFullScreen(false)
-        fullscreenButton.setGraphic(new ImageView(fullscreenButtonImage))
-      } else {
-        primaryStage.setFullScreen(true)
-        fullscreenButton.setGraphic(new ImageView(fullscreenReleaseButtonImage))
-      }
-    })
-
-    // 再生バー作成
-    val timeSlider = new Slider()
-    timeSlider.setOnMouseDragged(event => {
-      embeddedMediaPlayer.controls().setPosition((timeSlider.getValue/100).toFloat)
-    })
-    timeSlider.setOnMouseClicked(event => {
-      embeddedMediaPlayer.controls().setPosition((timeSlider.getValue/100).toFloat)
-    })
-
-    toolBarHBox.getChildren.addAll(timeLabel,backButton,pauseButton,forwardButton,fullscreenButton)
-    toolBarHBox.setAlignment(Pos.CENTER)
-
-    toolBar.setAlignment(Pos.BOTTOM_CENTER)
-    toolBar.getChildren.addAll(timeSlider,toolBarHBox)
-
-
-    // タイトルバー作成
-    // 透明なブロックを一番上においてウィンドウを動かせるようにする
-    val rect1 = new Rectangle(0,0,0,40)
-    rect1.setFill(Color.TRANSPARENT)
-    rect1.widthProperty().bind(primaryStage.widthProperty())
-
-    // 終了ボタン
-    val exitButtonImage = new Image(getClass.getResourceAsStream("exit.png"))
-    val exitButton = new Button()
-    exitButton.setGraphic(new ImageView(exitButtonImage))
-    exitButton.setStyle("-fx-background-color:Black;-fx-background-radius:0")
-    exitButton.setOnMouseClicked(event => { // 押されたときにアプリを終了する
-      Platform.exit()
-    })
-
-    // アイコン化(最小化？言い方がよくわからない）
-    val iconifiedButtonImage = new Image(getClass.getResourceAsStream("iconified.png"))
-    val iconifiedButton = new Button()
-    iconifiedButton.setGraphic(new ImageView(iconifiedButtonImage))
-    iconifiedButton.setStyle("-fx-background-color:Black;-fx-background-radius:0")
-    iconifiedButton.setOnMouseClicked(event => {
-      primaryStage.setIconified(true)
-      videoStack.requestFocus()
-    })
-
-    // ウィンドウの最大化、最大化を元に戻す
-    val maximizedButtonImage = new Image(getClass.getResourceAsStream("maximized.png"))
-    val minimizedButtonImage = new Image(getClass.getResourceAsStream("minimized.png"))
-    val maximizedButton = new Button()
-    val primaryScreenBounds = Screen.getPrimary.getVisualBounds
-    maximizedButton.setGraphic(new ImageView(maximizedButtonImage))
-    maximizedButton.setStyle("-fx-background-color:Black;-fx-background-radius:0")
-    maximizedButton.setOnMouseClicked(event => {
-      if(primaryStage.isMaximized) {
-        primaryStage.setMaximized(false)
-        maximizedButton.setGraphic(new ImageView(maximizedButtonImage))
-      } else {
-        primaryStage.setMaximized(true)
-        primaryStage.setX(0)
-        primaryStage.setY(0)
-        primaryStage.setWidth(primaryScreenBounds.getWidth)
-        primaryStage.setHeight(primaryScreenBounds.getHeight)
-        maximizedButton.setGraphic(new ImageView(minimizedButtonImage))
-      }
-      videoStack.requestFocus()
-    })
-
-    // マウスがある時以外タイトルバーを消す(透明になっている)
-    // マウスがタイトルバーに入った時タイトルバーを表示
-    titleBar.setOnMouseEntered(event => {
-      titleBar.setOpacity(1)
-    })
-    // マウスがタイトルバーから離れたときタイトルバー非表示
-    titleBar.setOnMouseExited(event => {
-      titleBar.setOpacity(0)
-    })
-
-    // 重なって押せない要素を押せるようにする
-    titleButton.setPickOnBounds(false)
-    titleBar.setPickOnBounds(false)
-
-    titleButton.setAlignment(Pos.TOP_RIGHT)
-    titleButton.getChildren.addAll(maximizedButton,iconifiedButton,exitButton)
-
-    titleBar.setAlignment(Pos.TOP_LEFT)
-    titleBar.getChildren.addAll(rect1,titleButton)
-
-    videoStack.getChildren.addAll(videoImageView,toolBar,titleBar)
-
     // 表示
     val scene = new Scene(videoStack)
     primaryStage.setTitle("polishplayer")
+
+
+    // ツールバー、タイトルバーの作成
+    val toolBar = ToolbarCreator.create(embeddedMediaPlayer, primaryStage, scene)
+    val titleBar = TitlebarCreator.create(primaryStage, videoStack)
+    videoStack.getChildren.addAll(videoImageView,toolBar,titleBar)
 
     // ドラッグ処理
     scene.setOnDragOver(new EventHandler[DragEvent] {
@@ -227,28 +89,13 @@ class Main extends Application {
     primaryStage.setScene(scene)
     primaryStage.show()
 
-    // イベント処理
-    embeddedMediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter(){
-      // 時間表示
-      override def timeChanged(mediaPlayer: MediaPlayer, newTime: Long): Unit = {
-        Platform.runLater(() => {
-          timeLabel.setText(timeFmt.fmt(newTime))
-        })
-        timeSlider.setValue((newTime.toDouble/embeddedMediaPlayer.status().length().toDouble)*100)
-      }
-    })
-
-    // 全画面モードに入ったときに表示するテキストを空にしてる
+    // 全画面モードに入ったときに表示されるテキストを空にしてる
     primaryStage.setFullScreenExitHint("")
 
     // キーイベント処理
     scene.setOnKeyPressed(e => {
       e.getCode match {
-        case KeyCode.SPACE => embeddedMediaPlayer.controls().pause();if (embeddedMediaPlayer.status().isPlaying){
-          pauseButton.setGraphic(new ImageView(playButtonImage))
-        } else {
-          pauseButton.setGraphic(new ImageView(stopButtonImage))
-        }
+        case KeyCode.SPACE => embeddedMediaPlayer.controls().pause()
         case KeyCode.RIGHT => embeddedMediaPlayer.controls().skipTime(10000)
         case KeyCode.LEFT => embeddedMediaPlayer.controls().skipTime(-10000)
         case KeyCode.UP => embeddedMediaPlayer.audio().setVolume(embeddedMediaPlayer.audio().volume()+5)
@@ -256,15 +103,6 @@ class Main extends Application {
         case KeyCode.M => embeddedMediaPlayer.audio().mute()
         case KeyCode.F11 => primaryStage.setFullScreen(true)
       }
-    })
-
-
-    // ウィンドウ移動
-    rect1.setOnMousePressed(pressEvent => {
-      rect1.setOnMouseDragged(dragEvent => {
-        primaryStage.setX(dragEvent.getScreenX - pressEvent.getSceneX)
-        primaryStage.setY(dragEvent.getScreenY - pressEvent.getSceneY)
-      })
     })
 
     // 画面クリックイベント
