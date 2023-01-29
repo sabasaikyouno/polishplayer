c386b5244da266eb8ff0aeb3d660c4f6b7568818
sabasaikyouno <0852kuro@gmail.com>
2023-01-29 23:42:18 +0900

    keySettings

--- src/main/scala/event/KeyMouseEvent.scala
+++ src/main/scala/event/KeyMouseEvent.scala
@@ -10,6 +10,7 @@ import scalafx.scene.image.ImageView
 import scalafx.scene.input.KeyCode
 import scalafx.scene.layout.{StackPane, VBox}
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer
+import utils.KeySettings.keySetting
 
 object KeyMouseEvent {
 
@@ -25,7 +26,7 @@ object KeyMouseEvent {
 
   private def setKeyEvent(scene: Scene)(implicit stage: PrimaryStage, embeddedMediaPlayer: EmbeddedMediaPlayer) =
     scene.onKeyPressed = keyEvent =>
-      keyConf(keyEvent.getCode) match {
+      keySetting(keyEvent.getCode) match {
         case ("Pause", _) => pause()
         case ("FastForward", _) => fastForward()
         case ("Rewind", _) => rewind()
@@ -36,10 +37,6 @@ object KeyMouseEvent {
         case (_, digit) if digit.isDigitKey => skipRate(digit.getName.toFloat / 10)
         case _ => ()
       }
-
-  private def keyConf(keyCode: KeyCode): (String, KeyCode) = {
-    (keyCode.name, keyCode)
-  }
 
   private def setMouseEvent(toolBar: VBox, timeThumbnail: ImageView)(implicit videoStack: StackPane) = {
     videoStack.onMouseReleased = mouseEvent =>
---
+++ src/main/scala/utils/KeySettings.scala
@@ -0,0 +1,25 @@
+package utils
+
+import java.io.File
+
+import scalafx.scene.input.KeyCode
+import com.github.tototoshi.csv.CSVReader
+
+object KeySettings {
+
+  def keySetting(keyCode: KeyCode): (String, KeyCode) =
+    (findKeySettings(keyCode).fold("")(_.apply(1)), keyCode)
+
+  private def getKeySettings = {
+    val keySettingsFile = new File("src\\main\\resources\\key_settings.csv")
+    val reader = CSVReader.open(keySettingsFile)
+
+    try
+      reader.all()
+    finally
+      reader.close()
+  }
+
+  private def findKeySettings(keyCode: KeyCode) =
+    getKeySettings.find(_.head == keyCode.name)
+}
