3be458ee2eeca90b312e359615ca215b2458dafe
sabasaikyouno <0852kuro@gmail.com>
2023-02-06 17:11:58 +0900

    write keySettings

--- src/main/scala/utils/KeySettings.scala
+++ src/main/scala/utils/KeySettings.scala
@@ -3,19 +3,35 @@ package utils
 import java.io.File
 
 import scalafx.scene.input.KeyCode
-import com.github.tototoshi.csv.CSVReader
+import com.github.tototoshi.csv.{CSVReader, CSVWriter}
 
 object KeySettings {
 
   def findKeySetting(keyCode: KeyCode): (String, KeyCode) =
     (helpFindKeySettings(keyCode).fold("")(_.apply(1)), keyCode)
 
-  def settingsMap =
+  def keySettingsMap =
     getKeySettings
       .groupBy(_(1))
       .mapValues(
         _.map(_.head)
       )
+
+  def keySettingsWrite(settingsMap: Map[String, List[String]]) = {
+    val keySettingsFile = new File("src\\main\\resources\\keySettings.csv")
+    val writer = CSVWriter.open(keySettingsFile)
+
+    try
+      writer.writeAll(settingsMapToList(settingsMap))
+    finally
+      writer.close()
+  }
+
+  private def settingsMapToList(settingsMap: Map[String, List[String]]) = {
+    settingsMap.map { case (event, keyList) =>
+      keyList.flatMap(List(_, event))
+    }.toList
+  }
 
   private def getKeySettings = {
     val keySettingsFile = new File("src\\main\\resources\\keySettings.csv")
