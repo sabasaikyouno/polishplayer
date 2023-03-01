9b299634ccf36f211de70f877845fedde6cf8028
sabasaikyouno <0852kuro@gmail.com>
2023-03-01 16:49:39 +0900

    media info 0.2

--- src/main/scala/creator/MediaInfoCreator.scala
+++ src/main/scala/creator/MediaInfoCreator.scala
@@ -6,15 +6,18 @@ import scalafx.scene.text.Text
 import scalafx.stage.Stage
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer
 
+import scala.util.Try
+
 object MediaInfoCreator {
   def createMediaInfoStage(embeddedMediaPlayer: EmbeddedMediaPlayer) =
     new Stage {
       title = "media info"
       scene = new Scene(createMediaInfoVBox(embeddedMediaPlayer))
+      onShowing = _ => scene = new Scene(createMediaInfoVBox(embeddedMediaPlayer))
     }
 
   private def createMediaInfoVBox(embeddedMediaPlayer: EmbeddedMediaPlayer) =
     new VBox(
-      new Text("embeddedMediaPlayer.media().info().mrl()")
+      new Text(Try(embeddedMediaPlayer.media().info().mrl()).getOrElse(""))
     )
 }
