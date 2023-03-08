b2e4034099977947fb443a75eb7f69e41339150a
sabasaikyouno <0852kuro@gmail.com>
2023-03-08 18:51:43 +0900

    pin 0.1

--- src/main/scala/creator/PinCreator.scala
+++ src/main/scala/creator/PinCreator.scala
@@ -1,5 +1,21 @@
 package creator
 
-object PinCreator {
+import scalafx.scene.Scene
+import scalafx.scene.control.{Button, TextField}
+import scalafx.scene.layout.VBox
+import scalafx.stage.Stage
 
+object PinCreator {
+  def createPinStage =
+    new Stage {
+      title = "pin"
+      scene = new Scene(createPinVBox)
+    }
+
+  def createPinVBox =
+    new VBox(
+      new TextField(),
+      new TextField(),
+      new Button("save")
+    )
 }
