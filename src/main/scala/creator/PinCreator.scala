1d178ba5133eba8b2d5d5b7b4bd08b0a2bf271c2
sabasaikyouno <0852kuro@gmail.com>
2023-03-09 18:13:17 +0900

    pin 0.2

--- src/main/scala/creator/PinCreator.scala
+++ src/main/scala/creator/PinCreator.scala
@@ -1,5 +1,6 @@
 package creator
 
+import event.PinEvent.pinWrite
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, TextField}
 import scalafx.scene.layout.VBox
@@ -12,10 +13,21 @@ object PinCreator {
       scene = new Scene(createPinVBox)
     }
 
-  def createPinVBox =
+  def createPinVBox = {
+    val tf = new TextField()
+    val startTimeTf, endTimeTf = new TextField()
+
     new VBox(
-      new TextField(),
-      new TextField(),
-      new Button("save")
+      tf,
+      startTimeTf,
+      endTimeTf,
+      saveButton(tf, startTimeTf, endTimeTf)
     )
+  }
+
+  private def saveButton(tf: TextField, startTimeTf: TextField, endTimeTf: TextField) =
+    new Button {
+      text = "save"
+      onMouseClicked = _ => pinWrite(tf.text.value, startTimeTf.text.value.toLong, endTimeTf.text.value.toLong)
+    }
 }
--- src/main/scala/event/PinEvent.scala
+++ src/main/scala/event/PinEvent.scala
@@ -1,5 +1,12 @@
 package event
 
-object PinEvent {
+import utils.CSVUtils.csvReadAll
 
+object PinEvent {
+  def pinWrite(text: String, startTime: Long, endTime: Long) = {
+
+  }
+
+  def getPins =
+    csvReadAll("src\\main\\resources\\pin.csv")
 }
